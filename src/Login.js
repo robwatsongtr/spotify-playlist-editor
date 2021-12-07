d1261274e2a957d40897ea00c1d30c031c6f4be1
robwatsongtr <rwatso@gmail.com>
2021-12-06 21:16:33 -0800

    completed login auth

--- src/Login.js
+++ src/Login.js
@@ -1,1 +1,17 @@
 // Login Component
+
+import React from 'react';
+
+function Login() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a className="btn-spotify" href="/auth/login" >
+                    Login with Spotify 
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default Login;
--- src/PlaylistApp.js
+++ src/PlaylistApp.js
@@ -1,1 +1,16 @@
-// main playlist app component
+import React, { useState, useEffect } from 'react';
+
+function PlaylistApp(props) {
+
+   return (
+      <>
+        <div className="container">
+          <div className="main-wrapper">
+            this is the main app
+          </div>
+        </div>
+      </>
+    );
+}
+
+export default PlaylistApp
