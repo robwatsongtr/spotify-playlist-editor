ba03249c6002fed464ea1e725466e4de3e1ce0b8
robwatsongtr <rwatso@gmail.com>
2021-12-09 21:26:54 -0800

    created overall layout

--- src/PlaylistApp.js
+++ src/PlaylistApp.js
@@ -6,6 +6,12 @@ function PlaylistApp(props) {
    return (
       <>
         <Navbar />
+
+        <div className="appLayout">
+            <div className="box-1">Box 1</div>
+            <div className="box-2">Box 2</div>
+            <div className="box-3">Box 3</div>
+        </div>
       </>
     );
 }
