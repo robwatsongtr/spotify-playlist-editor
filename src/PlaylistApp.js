3cf22a34773b41015aac1a255b545b3cb84822ce
robwatsongtr <rwatso@gmail.com>
2021-12-14 23:30:00 -0800

    working on hooks

--- src/PlaylistApp.js
+++ src/PlaylistApp.js
@@ -4,24 +4,34 @@ import ListPlaylists from './components/ListPlaylists'
 import SourcePlaylist from './components/SourcePlaylist'
 import DestinationPlaylist from './components/DestinationPlaylist'
 import Player from './components/Player'
+import axios from 'axios'
+import SpotifyPlayer from 'react-spotify-web-playback'
 
 const PlaylistApp = (props) => {
 
   const currentToken = props.token; 
 
-   return (
-      <>
-        <Navbar />
+  const [currentUserID, setCurrentUserID] = useState( { userId: ''} )
 
-        <div className="appLayout">
-            <ListPlaylists />
-            <SourcePlaylist />
-            <DestinationPlaylist />
-            <Player />
-        </div>
+  const [userPlaylists, setUserPlaylists] = useState({
+    selectedPlaylist: '',
+    listofPlaylistsFromApi: []
+  })
+  
 
-      </>
-    );
+  return (
+    <>
+      <Navbar />
+
+      <div className="appLayout">
+          <ListPlaylists />
+          <SourcePlaylist />
+          <DestinationPlaylist />
+          <Player />
+      </div>
+
+    </>
+  );
 }
 
 export default PlaylistApp
